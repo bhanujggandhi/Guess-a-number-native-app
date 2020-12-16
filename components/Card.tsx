94115d484ebe956044b41e5b8d9be4d9277a3753
bhanujggandhi <gandhibhanuj@gmail.com>
2020-12-16 23:06:35 +0530

    StartScreen added with a card component with styles

---
+++ components/Card.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import * as CSS from "csstype";
+
+type CardProps = {
+  children: React.ReactNode;
+  style: object;
+};
+
+const Card = (props: CardProps) => {
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 15,
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+  },
+});
+
+export default Card;
